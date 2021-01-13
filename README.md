c1b3f8b570555e8c12175000f24905116a5a51fa
Snax <shanni.c.suissa@gmail.com>
2021-01-12 16:51:22 -0800

    finishedgit add -A! and it works, the readme used here was generated with the project

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# readme-generator-hw-9
+# Good Read Me Generator
+    ![GitHub License](https://img.shields.io/badge/License-None-blue)
+  
+    ## Description
+    This app allows you to quickly and easily put together a README for your projects. 
+    <br>
+    <br>
+  
+    ## Table of Contents
+    * [Installation](#Installation)
+    * [Usage](#Usage)
+    * [License](#License)
+    * [Contributing](#Contributing)
+    * [Tests](#Tests)
+    * [Questions](#Questions)
+   <br>
+   <br>
+  
+    ## Installation
+    To install dependencies, run the following commend:
+```
+npm i
+```
+    
+    <br>
+    <br>
+  
+    ## Usage
+    Run 'node index.js' in order to get the generator going. 
+    <br>
+    <br>
+  
+    ## License
+    This project is licensed under the None license.
+    <br>
+    <br>
+  
+    ## Questions
+    If you have any questions about the repo, open an issue, or please feel free to contact me directly at shanni.c.suissa@gmail.com. 
+    <br>
+    <br>
+    
+    More of my work can be found on GitHub at: [ShanniSnax](https://github.com/ShanniSnax/).
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,13 +1,58 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+    ![GitHub License](https://img.shields.io/badge/License-${data.license.replace(" ","%20")}-blue)
   
-`
-  // get readme from online and delete content use above format ^^
+    ## Description
+    ${data.description}
+    <br>
+    <br>
+  
+    ## Table of Contents
+    * [Installation](#Installation)
+    * [Usage](#Usage)
+    * [License](#License)
+    * [Contributing](#Contributing)
+    * [Tests](#Tests)
+    * [Questions](#Questions)
+   <br>
+   <br>
+  
+    ## Installation
+    To install dependencies, run the following commend:`
+    +
+    "\n```\n"
+    +
+    data.installCommand
+    +
+    "\n```\n"
+    +
+    `
+    <br>
+    <br>
+  
+    ## Usage
+    ${data.usage}
+    <br>
+    <br>
+  
+    ## License
+    This project is licensed under the ${data.license} license.
+    <br>
+    <br>
+  
+    ## Questions
+    If you have any questions about the repo, open an issue, or please feel free to contact me directly at ${data.email}. 
+    <br>
+    <br>
+    
+    More of my work can be found on GitHub at: [${data.githubUser}](https://github.com/${data.githubUser.replace(" ","%20")}/).`;
+  
+      return template;
+    }
+  
+  module.exports = generateMarkdown;
+  
 
 
-;
-}
 
-module.exports = generateMarkdown;
